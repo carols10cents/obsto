f1a6702b0be48ea3a88170e49cd74c218aea68b4
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2024-07-11 13:08:17 -0400

    Try increasing the client timeout; this fixed it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use futures_util::{stream, StreamExt};
-use object_store::{aws::AmazonS3Builder, ObjectStore};
-use std::sync::Arc;
+use object_store::{aws::AmazonS3Builder, ClientOptions, ObjectStore};
+use std::{sync::Arc, time::Duration};
 
 #[tokio::main]
 async fn main() {
@@ -11,6 +11,7 @@ async fn main() {
         .with_bucket_name(std::env::var("AWS_BUCKET").unwrap())
         .with_access_key_id(std::env::var("AWS_ACCESS_KEY_ID").unwrap())
         .with_secret_access_key(std::env::var("AWS_SECRET_ACCESS_KEY").unwrap())
+        .with_client_options(ClientOptions::new().with_timeout(Duration::from_secs(60)))
         .with_imdsv1_fallback();
 
     let object_store = Arc::new(builder.build().expect("valid object store"));
